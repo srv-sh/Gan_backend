ee61ff56e455fb4367207b148e7756a13a1e9659
rukaiyafahmida <rukaiyafahmida7@gmail.com>
2022-12-27 12:25:19 +0600

    added env variables

--- utils.py
+++ utils.py
@@ -8,14 +8,15 @@ from datetime import datetime as dt
 class Fashion_Generator_Handler():    
     def __init__(self):
         self.s3 = boto3.resource(service_name = 's3',
-                                region_name = 'ap-south-1',
-                                aws_access_key_id = 'AKIAV3NQCJC5BYKAI4X7',
-                                aws_secret_access_key='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')
+                                region_name = config("AWS_REGION_NAME"),
+                                aws_access_key_id = config("AWS_ACCESS_KEY_ID"),
+                                aws_secret_access_key=config("AWS_SECRET_ACCESS_KEY"))
         self.s3_bucket = self.s3.Bucket(config("BUCKET"))
+        self.bucket_url = config("BUCKET_URL")
 
     def upload_to_s3_bucket(self,bucket, filepath, key:str):
             bucket.upload_fileobj(filepath,key)
-            return 'https://shelly-ml-data-bucket.s3.ap-south-1.amazonaws.com/'+key
+            return self.bucket_url+key
 
     ## FUNCTION TO GENERATE IMAGE
     def generate_img(self,prompt):
